7e60aaf58da4d53a45eccca67526d0bc276592d4
Nicolás Navarro <nicohnavarro@gmail.com>
2024-08-22 16:36:49 +0200

    Adding new structure 🧱

---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
